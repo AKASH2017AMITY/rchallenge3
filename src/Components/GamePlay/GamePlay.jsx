9df8461a61397f599354b66a390f1ccd3871447e
akash kumar <120650503+AKASH2017AMITY@users.noreply.github.com>
2023-08-10 20:30:41 +0530

    third

--- src/Components/GamePlay/GamePlay.jsx
+++ src/Components/GamePlay/GamePlay.jsx
@@ -1,32 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './GamePlay.module.css'
 
 const GamePlay = () => {
+
+    const[selectedNumber,setSelectedNumber]=useState();
+    let arrNumber =[1,2,3,4,5,6];
+    
+    // if(selectedNumber){
+    //     const className+={styles.activeButton}
+    // }
+
+    
     return (
         <div className={styles.container}>
             <header className={styles.header}>
                 <div className={styles.leftHeader}>
-                    <h1>0</h1>
+                    <h1 className={styles.heading}>0</h1>
                     <p className={styles.para}>Total Score</p>
                 </div>
                 <div className={styles.rightHeader}>
                     <div>
-                        <button>0</button>
-                        <button>1</button>
-                        <button>2</button>
-                        <button>3</button>
-                        <button>4</button>
-                        <button>5</button>
-                        <button>6</button>
+                        {arrNumber.map((value,i)=>
+                            <button isSelected={value===selectedNumber} className={styles.button} key={i} onClick={()=>setSelectedNumber(value)}>{value}</button>
+                        )}
                     </div>
                     <div >
                         <p className={styles.p}>Select Number</p>
                     </div>
                 </div>
             </header>
-            <div>
-
-            </div>
+            <main id={styles.main}>
+                <div>
+                    <img className={styles.mainImage}src="images/cubes.jpg" alt="" />
+                </div>
+                <div>
+                    <p className={styles.mainText}>click on the dice to roll</p>
+                </div>
+                <div>
+                    <button className={styles.mainButton}>Reset score</button>
+                </div>
+                <div>
+                    <button className={styles.mainButton1}>show rules</button>
+                </div>
+            </main>
         </div>
     )
 }
